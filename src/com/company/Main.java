9e779ffc1b141a6d9c16ad1f594fec9ab6fcb995
DESKTOP-231T7BN\Maciek <m.bandzarzewski>
2018-01-29 20:14:36 +0100

    Create ship board

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Przykladowy tekst");
+        printLetters();
+        printBoard();
+           }
+     static void printBoard(){
+        for (int i = 1; i <= 10; i++) {
+            if (i < 10) {
+                System.out.print(' ');
+            }
+            System.out.print(i);
+            for (int j = 0; j < 10; j++) {
+                char shipValue=getRandomShip();
+                System.out.print(shipValue);
+            }
+            System.out.print('\n');
+        }
+     }
+
+    private static char getRandomShip() {
+        if (Math.random() < 0.2) {
+            return 'O';
+        } else {
+            return ' ';
+        }
+    }
+    static void printLetters(){
+        System.out.print("   ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print((char) ('A' + i));
+        }
+        System.out.print('\n');
     }
 }
